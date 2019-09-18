967286b05c540f8185a211d8857621e2716e837a
20125 <20125@MININT-HADV8T3.columbushs.com>
2019-09-18 14:16:12 -0400

    done

--- song/src/song/Song.java
+++ song/src/song/Song.java
@@ -16,14 +16,38 @@ public class Song {
      */
     public static void main(String[] args) {
         // Code inside the main method
-        
-        
-        
-            
+        System.out.println("Didn\'t know what time it was the lights were low \n "
+                + "    I leaned back on my radio \n"
+                + "Some cat was layin down some rock \'n\' roll, lotta soul, he said\n"
+                + "Then the loud sound did seem to fade\n"
+                + "Came back like a slow voice on a wave of phase\n"
+                + "That weren\'t no DJ, that was hazy cosmic jive");
+        chorus();
+
+        System.out.println("I had to phone someone so I picked on you\n"
+                + "Hey, that's far out, so you heard him too\n"
+                + "Switch on the TV, we may pick him up on channel two\n"
+                + "Look out your window, I can see his light\n"
+                + "If we can sparkle he may land tonight\n"
+                + "Don't tell your poppa or he'll get us locked up in fright");
+        chorus();
+        chorus();
     }
-    
-    public static void chorus(){
-        // Chorus lyrics go here
+
+    public static void chorus() {
+        // Chorus lyrics go here 
+        System.out.println("There's a starman waiting in the sky\n"
+                + "He'd like to come and meet us\n"
+                + "But he thinks he'd blow our minds\n"
+                + "There's a starman waiting in the sky\n"
+                + "He's told us not to blow it\n"
+                + "'Cause he knows it's all worthwhile\n"
+                + "He told me\n"
+                + "Let the children lose it\n"
+                + "Let the children use it\n"
+                + "Let all the children boogie");
+
     }
-    
+
+ 
 }
